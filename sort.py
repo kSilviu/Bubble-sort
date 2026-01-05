00733106b0bcee266d2959affe46583babe5e881
Silviu Hostinariu <silviuhostinariumihai@gmail.com>
2026-01-05 08:00:16 +0000

    bubble sort function

---
+++ sort.py
@@ -0,0 +1,14 @@
+def bubbleSort(x):
+
+    indexNumber = len(x)-1
+
+    for i in indexNumber(n):
+        swapped = False
+
+        for h in range(0, n-i-1):
+            if x[h] > x[h+1]:
+                x[h], x[h+1] = x[h+1], x[i]
+                swapped = True
+        if (swapped = False):
+            break
+
