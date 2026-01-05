ba5b5d486f8e4518800ef2961f748b8851e44d69
Silviu Hostinariu <silviuhostinariumihai@gmail.com>
2026-01-05 08:05:20 +0000

    more fixes (┬┬﹏┬┬)

--- sort.py
+++ sort.py
@@ -5,7 +5,7 @@ def bubbleSort(x):
     for i in range(indexNumber):
         swapped = False
 
-        for h in range(0, n-i-1):
+        for h in range(0, indexNumber-i-1):
             if x[h] > x[h+1]:
                 x[h], x[h+1] = x[h+1], x[i]
                 swapped = True
